4a70eb026abd2b21de61e040dfe6f144b5a7df92
Roy Hodge <roy.hodge@mail.com>
2018-07-21 22:50:15 -0600

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,183 @@
+"use strict";
+
+const assert = require('assert');
+var address1;
+var issuedata;
+var listassets;
+var totalbalance;
+var listwallettransactions;
+const connection = {
+    port: "8340",
+    host: '127.0.0.1',
+    user: "multichainrpc",
+    pass: "2Cvvg7ufaNG2i7pG4SZegP4YqYHCdA9X76FZ8WthJLZG"
+}
+const bluebird = require("bluebird");
+console.log("Connecting....")
+const multichain = bluebird.promisifyAll(require("../index.js")(connection), {suffix: "Method"});
+console.log("***********Connection established*********")
+
+function getInfo(req,resp) {
+    console.log("Calling Get info Method...");
+   // return true;
+multichain.getInfo((err, res) => {
+        console.log(res);
+    })
+}
+//list all assets method
+
+ /* function listAssets(req,resp)
+{
+    console.log("Calling List Assets Method...");
+   // return true;
+    multichain.listAssetsMethod()
+	.then (listassetsresp => {
+		assert(listassetsresp , "Could not get new address")
+		listassets=listassetsresp ;
+		console.log(listassets);
+	})
+
+}  */
+
+//Get new address and validate it
+
+/* function getNewVarifiedaddress()
+{
+console.log("Calling Get New Address Method...");
+	 multichain.getNewAddressMethod()
+      .then(address => {
+        assert(address, "Could not get new address")
+        address1 = address;
+		console.log(address1)
+        return multichain.validateAddressMethod({address: address1})
+    })
+
+}  */
+
+  function getTotalBalance()
+{
+	multichain.getTotalBalancesMethod({minconf: 1})
+	.then (totalbalanceresp => {
+		assert(totalbalanceresp, "Could not get new address")
+		totalbalance=totalbalanceresp;
+		console.log(totalbalance);
+	})
+}
+
+
+/* function listWalletTransactions()
+{
+	multichain.listWalletTransactionsMethod({count: 10})
+	.then(wallettransresp => {
+		assert(wallettransresp, "Could not wallet transactions")
+		listwallettransactions=wallettransresp;
+		console.log(listwallettransactions);
+	})
+} */
+
+// issue Assets
+
+/* function createAssets ()
+{
+console.log("Calling Issue Assets Method...");
+multichain.issueMethod(
+		 issuedata = {
+			address: "133H9FFJ9Uhbivv97QZgHTDzQh1D2e8YPCMvmv",
+            asset: { name: "ashish2",open: true },
+            qty: 1000,
+            units: 0.1
+        }, (err, res) => {
+    console.log('Txn id of Block is  : ' +res)
+})
+}  */
+
+/* function sendAssets ()
+{
+console.log("Calling Send Assets Method...");
+multichain.sendAssetFromMethod({
+            from: "1bWzkMJ6cicQjDA4yi86favKX15tmJy1dkEGBP",
+            to: "1bWzkMJ6cicQjDA4yi86favKX15tmJy1dkEGBP",
+            asset: "asset_chk1",
+            qty: 10
+        },(err, res) => {
+    console.log('Txn id of Block is  : ' +res)
+	console.log(err);
+		})
+
+}  */
+
+/* function sendAssetswithdata ()
+{
+console.log("Calling Send Assets Method...");
+multichain.sendWithMetadataFromMethod({
+            from: "1bWzkMJ6cicQjDA4yi86favKX15tmJy1dkEGBP",
+            to: "1E3d1Zs9JBLPPU8oqPvs7fMwXSxmjb81LLU8JA",
+			//1bK2FjwY3kUhVj4wxDf1M1pqZgczufmVd1cUKF
+            amount: {"asset_chk1":10},
+            data: new Buffer("a nice message, for you").toString("hex")
+        },(err, res) => {
+    console.log('Txn id of Block is  : ' +res)
+		})
+
+} */
+/* {
+	"from":"hgj",
+	"to":"hgjh",
+	"amount":"hgmn:67",
+	"data":"gjh"
+} */
+/* {
+"from":"1bWzkMJ6cicQjDA4yi86favKX15tmJy1dkEGBP",
+"to":"1bK2FjwY3kUhVj4wxDf1M1pqZgczufmVd1cUKF",
+"amount":"asset_chk1:10",
+"data":"pli"
+} */
+/*  function grantAddress()
+{
+console.log("Calling Grant Method...");
+	multichain.grantMethod({
+            addresses: "1VL98y8Dgyrd7syQa34wzjtDU8jRkrxDaYuzax",
+            permissions: "send"
+        })
+}  */
+
+/* function issueAssets(data)
+{
+	 multichain.getNewAddressMethod()
+    .then(address => {
+        assert(address, "Could not get new address")
+        address1 = address;
+
+        return multichain.validateAddressMethod({address: address1})
+    })
+    .then(privateKey => {
+        assert(privateKey)
+        return multichain.grantMethod({
+            addresses: address1,
+            permissions: "send,receive,issue,admin"
+        })
+    })
+	.then(revokeTxid => {
+        assert(revokeTxid);
+		console.log("Issue Asset Started")
+        return multichain.issueMethod(
+		issuedata = {
+			address: address1,
+            asset: { name: "test1",open: true },
+            qty: 1000,
+            units: 0.1
+        }, (err, res) => {
+    console.log('Txn id of Block is  : ' +res)
+	})
+    })
+} */
+getInfo()
+/* createAssets() */
+/* getNewVarifiedaddress() */
+/* issueAssets () */
+ //listAssets()
+/* grantAddress() */
+//sendAssets ()
+/* sendAssetswithdata() */
+getTotalBalance()
+/* listWalletTransactions() */
---
+++ main.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const electron = require('electron')
+const app = electron.app
+const globalShortcut = electron.globalShortcut
+const os = require('os')
+const path = require('path')
+const config = require(path.join(__dirname, 'package.json'))
+const BrowserWindow = electron.BrowserWindow
+const globalConfig = require(path.join(__dirname, 'config.json'))
+
+app.setName(config.productName)
+
+var fs = require('fs');
+
+let multichain = require("multichain-node")({
+    port: globalConfig.connection.port,
+    host: globalConfig.connection.host,
+    user: globalConfig.connection.user,
+    pass: globalConfig.connection.pass,
+});
+
+multichain.getInfo((err, info) => {
+    if(err){
+        throw err;
+    }
+    JSON.stringify(info);
+    console.log(info);
+    global.x = (info);
+});
+
+var mainWindow = null
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({
+    title: config.productName,
+backgroundColor: '#312450',
+    webPreferences: {
+      nodeIntegration: true,
+      defaultEncoding: 'UTF-8'
+    }
+  })
+
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+
+  // Enable keyboard shortcuts for Developer Tools on various platforms.
+  let platform = os.platform()
+  if (platform === 'darwin') {
+    globalShortcut.register('Command+Option+I', () => {
+      mainWindow.webContents.openDevTools()
+    })
+  } else if (platform === 'linux' || platform === 'win32') {
+    globalShortcut.register('Control+Shift+I', () => {
+      mainWindow.webContents.openDevTools()
+    })
+  }
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.setMenu(null)
+    mainWindow.show()
+  })
+
+  mainWindow.onbeforeunload = (e) => {
+    // Prevent Command-R from unloading the window contents.
+    e.returnValue = false
+  }
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+})
+
+app.on('window-all-closed', () => { app.quit() })
